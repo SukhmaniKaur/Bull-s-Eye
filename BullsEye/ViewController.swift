833b9aad4a402d36ee391c779f02b6b069f26d99
SukhmaniKaur <sukhmanikaur.appknit@gmail.com>
2020-07-25 00:52:30 +0530

    calculated score and action added to start over btn

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -12,7 +12,11 @@ class ViewController: UIViewController {
     
     var currentValue: Int = 1
     var targetValue: Int = 0
+    var score = 0
+    var round = 1
 
+    @IBOutlet weak var roundLbl: UILabel!
+    @IBOutlet weak var scoreLbl: UILabel!
     @IBOutlet weak var targetLbl: UILabel!
     @IBOutlet weak var slider: UISlider!
     override func viewDidLoad() {
@@ -26,21 +30,31 @@ class ViewController: UIViewController {
         currentValue = 50
         targetValue = Int.random(in: 1...100)
         slider.value = Float(currentValue)
+        
     }
     
     func updateLabels() {
         targetLbl.text = "\(targetValue)"
+        scoreLbl.text = "\(score)"
+        roundLbl.text = "\(round)"
     }
 
     @IBAction func hitMeBtnIsPressed(_ sender: UIButton) {
+        
         let difference = abs(targetValue - currentValue)
-        let alert = UIAlertController(title: "Hello World!", message: "the value of slider is now: \(currentValue)" + "\nThe target value is: \(targetValue)" + "\n The difference is: \(difference)", preferredStyle: .alert)
+        let points = 100 - difference
+        score += points
+        let message = "Your score \(points) points."
+        let alert = UIAlertController(title: "Bull’s Eye", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "ÄweSome", style: .default) { (action) in
+            self.round += 1
             self.updateLabels()
+            self.startNewRound()
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
-        startNewRound()
+        print("the value of slider is now: \(currentValue)" + "\nThe target value is: \(targetValue)" + "\n The difference is: \(difference)")
+        
         
     }
     
@@ -49,5 +63,11 @@ class ViewController: UIViewController {
         currentValue = lroundf(slider.value)
     }
     
+    @IBAction func startOverBtnIsPressed(_ sender: UIButton) {
+        score = 0
+        round = 1
+        startNewRound()
+        updateLabels()
+    }
 }
 
