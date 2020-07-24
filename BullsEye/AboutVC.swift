32d226d75b8e12b4b37c884366dbf08a845415ef
SukhmaniKaur <sukhmanikaur.appknit@gmail.com>
2020-07-25 01:31:41 +0530

    designed aboutVC

---
+++ BullsEye/AboutVC.swift
@@ -0,0 +1,30 @@
+//
+//  AboutVC.swift
+//  BullsEye
+//
+//  Created by MACBOOK on 25/07/20.
+//  Copyright © 2020 SukhmaniKaur. All rights reserved.
+//
+
+import UIKit
+
+class AboutVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
